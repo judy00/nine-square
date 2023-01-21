dc067fda82d2d2982db6e0b71fc5ba20e669d152
JudyWu <judywu@sat.cool>
2023-01-22 02:32:23 +0800

    feat: 新增九宮格組件

---
+++ src/components/BallMoveToRight.vue
@@ -0,0 +1,32 @@
+<template>
+  <h3>綠球向右移動</h3>
+  <section class="container">
+    <div class="cell"></div>
+    <div class="cell"></div>
+    <div class="cell cell-flash"></div>
+    <div class="cell"></div>
+    <div class="cell cell-flash"></div>
+    <div class="cell"></div>
+    <div class="cell"></div>
+    <div class="cell"></div>
+    <div class="cell cell-flash"></div>
+  </section>
+</template>
+
+<style scoped>
+.cell:nth-child(1):after,
+.cell:nth-child(3):after,
+.cell:nth-child(7):after,
+.cell:nth-child(9):after {
+  animation: moveToRight 3s ease-in-out infinite;
+}
+
+@keyframes moveToRight {
+  0% {
+    left: 50%;
+  }
+  100% {
+    left: calc(50% + 500px);
+  }
+}
+</style>
---
+++ src/components/BallMoveToRightCorner.vue
@@ -0,0 +1,34 @@
+<template>
+  <h3>綠球向右下移動</h3>
+  <section class="container container-move-right-corner">
+    <div class="cell" style="--t: 200px; --l: 400px"></div>
+    <div class="cell"></div>
+    <div class="cell cell-flash" style="--t: 200px"></div>
+    <div class="cell"></div>
+    <div class="cell cell-flash"></div>
+    <div class="cell"></div>
+    <div class="cell" style="--l: 400px"></div>
+    <div class="cell"></div>
+    <div class="cell cell-flash"></div>
+  </section>
+</template>
+
+<style scoped>
+.cell:nth-child(1):after,
+.cell:nth-child(3):after,
+.cell:nth-child(7):after,
+.cell:nth-child(9):after {
+  animation: moveToRightCorner 1.5s ease-in-out infinite;
+}
+
+@keyframes moveToRightCorner {
+  0% {
+    top: 50%;
+    left: 50%;
+  }
+  100% {
+    top: calc(100% + var(--t, 0px));
+    left: calc(100% + var(--l, 0px));
+  }
+}
+</style>
